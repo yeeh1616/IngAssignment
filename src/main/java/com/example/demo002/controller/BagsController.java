b530a5aefebedc64350eba26a9d17c7f5bc62dd5
Ye <yeeh1616@gmail.com>
2022-09-24 22:12:20 +0200

    haha

--- src/main/java/com/example/demo002/controller/BagsController.java
+++ src/main/java/com/example/demo002/controller/BagsController.java
@@ -5,30 +5,39 @@ import com.example.demo002.services.BagsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
-@Controller
+@RestController
 public class BagsController {
     @Autowired
     private BagsService bagsService;
 
     @GetMapping("/bags")
-    public String bags(Model model){
-        Iterable<Bag> bags = bagsService.listAllBags();
-        Bag bag = new Bag();
-        model.addAttribute("bags", bags);
-        model.addAttribute("bag", bag);
-        return "bags";
+    public Iterable<Bag> bagsRest(Model model, @RequestParam(name="bagsNum",defaultValue="3") String bagsNum){
+        return bagsService.listAllBags();
     }
 
     @PostMapping("/add")
-    public String submitForm(@ModelAttribute("user") Bag bag) {
+    public Bag addRest(@RequestBody Bag bag) {
         bagsService.addBag(bag);
-        System.out.println(bag);
-        return "redirect:/bags";
+
+        return bag;
     }
+
+//    @GetMapping("/bags")
+//    public String bags(Model model){
+//        model.addAttribute("bags", bagsService.listAllBags());
+//        model.addAttribute("bag", new Bag());
+//
+//        return "bags";
+//    }
+//
+//    @PostMapping("/add")
+//    public String add(@ModelAttribute("bag") Bag bag) {
+//        bagsService.addBag(bag);
+//
+//        return "redirect:/bags";
+//    }
 }
