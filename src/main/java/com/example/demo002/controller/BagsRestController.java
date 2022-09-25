3c16657de96c16cc2b2c2d53f428434d7d3dc232
Ye <yeeh1616@gmail.com>
2022-09-25 10:49:26 +0200

    haha

--- src/main/java/com/example/demo002/controller/BagsRestController.java
+++ src/main/java/com/example/demo002/controller/BagsRestController.java
@@ -3,41 +3,45 @@ package com.example.demo002.controller;
 import com.example.demo002.entities.Bag;
 import com.example.demo002.services.BagsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.*;
+/*
+http://127.0.0.1:8080/bags
+http://127.0.0.1:8080/bags?bagsNum=10
+http://127.0.0.1:8080/bagsui
+
+Add a bag:
+http://127.0.0.1:8080/bags
+-- in postman, select Body, raw and JSON, input the following data in Json:
+{
+    "apple_num": 2,
+    "supplier": "ROYAL_GALA",
+    "time": "2022-09-24T20:10:02.890+00:00",
+    "price": 5
+}
+* */
 
 @RestController
+@RequestMapping("/bags")
 public class BagsRestController {
     @Autowired
     private BagsService bagsService;
 
-    @GetMapping("/bags")
-    public Iterable<Bag> bagsRest(Model model, @RequestParam(name="bagsNum",defaultValue="3") Integer bagsNum){
+    @GetMapping()
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public Iterable<Bag> getList(Model model, @RequestParam(name="bagsNum",defaultValue="3") Integer bagsNum){
         return bagsService.listAllBags(bagsNum);
     }
 
-    @PostMapping("/add")
-    public Bag addRest(@RequestBody Bag bag) {
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseBody
+    public Bag add(@RequestBody Bag bag) {
         bagsService.addBag(bag);
 
         return bag;
     }
-
-//    @GetMapping("/bags")
-//    public String bags(Model model){
-//        model.addAttribute("bags", bagsService.listAllBags());
-//        model.addAttribute("bag", new Bag());
-//
-//        return "bags";
-//    }
-//
-//    @PostMapping("/add")
-//    public String add(@ModelAttribute("bag") Bag bag) {
-//        bagsService.addBag(bag);
-//
-//        return "redirect:/bags";
-//    }
 }
--- src/main/java/com/example/demo002/services/BagsServiceImpl.java
+++ src/main/java/com/example/demo002/services/BagsServiceImpl.java
@@ -5,9 +5,6 @@ import com.example.demo002.repository.BagRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
-import java.util.ArrayList;
-
 @Service
 public class BagsServiceImpl implements BagsService {
     @Autowired
